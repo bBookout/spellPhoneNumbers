f3f956ffef357eeeb28e37d3d82728a5eba6f395
Brian Bookout <brianbookout4@gmail.com>
2016-07-19 21:18:08 -0700

    first commit
    An app that allows a user to enter a number in text, hit a button, and see corresponding letters in a listbox.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace spellPhoneNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please type a phone number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 65);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " Hit the button below to see the letter\r\n combinations from telephone buttons\r\n t" +
+    "hat correspond to the numbers that \r\n you entered. They will appear in the \r\n bo" +
+    "x to the right.";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(201, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 77);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "See letters";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(409, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(301, 264);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 328);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace spellPhoneNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        string phoneNumber = null;  //declare an empty string placeholder for the number entered in the textbox
+              
+        //create a dictionary for all the possible letters on each number
+        Dictionary<string, string> PossibleLetters = new Dictionary<string, string> {
+            {"1", " " },
+            {"2", "abc" },
+            {"3", "def" },
+            {"4", "ghi" },
+            {"5", "jkl" },
+            {"6", "mno" },
+            {"7", "pqrs" },
+            {"8", "tuv" },
+            {"9", "wxyz" },
+            {"0", " " },
+        };
+
+        //create a list of strings, cycle through each number, get each possible letter then return them
+        public List<string> FromPhoneNumber(string phoneNumber)
+        {
+            var result = new List<string> { string.Empty };
+
+            // cycle through each number in the phone number
+            for (int i = 0; i < phoneNumber.Length; i++)
+            {
+                var phoneNumList = new List<string>();
+                foreach (var v in result)
+                {
+                    var letters = PossibleLetters[phoneNumber[i].ToString()];
+                    // go through each possible letter of the number
+                    for (int j = 0; j < letters.Length; j++)
+                    {
+                        phoneNumList.Add(v + letters[j]); //add them to the list                       
+                    }
+                }
+                    result = phoneNumList; //result list becomes the list that we've added each letter combo to                
+            }
+                return result;//return the resulting list
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            phoneNumber = textBox1.Text;//set the phoneNumber string to the textbox text (number the user enters)                        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(phoneNumber))//check for an empty string
+                {
+                    MessageBox.Show("Please enter a phone number without dashes. Numbers 0 and 1 will not have letter values.");
+                    return;
+                }
+                //other validation could be done here ie. regEx search for special characters
+                else
+                {
+                    listBox1.DataSource = (FromPhoneNumber(phoneNumber));//populate Listbox with string collection
+                }                                                       //that is returned from function
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please try again. Enter a number without special characters.\n\n" + ex);
+            }
+        }
+       
+    }
+}
